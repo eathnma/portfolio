ac525cda296abab633d2ae093dc627082247bbd2
eathnma <ethanmahc@gmail.com>
2021-09-21 17:57:05 -0700

    added play button to video elements

--- public/js/video.js
+++ public/js/video.js
@@ -1,22 +1,14 @@
-// grab all video elements
-var video = document.querySelectorAll("video");
-
-video.onmouseenter = function () {
-  mouseEnter();
-};
-
-function mouseEnter() {
-  console.log("mouse has entered");
-}
-
 // loop through all the videos onclick
-for (let i = 0; i < video.length; i++) {
-  video[i].addEventListener("click", function () {
-    console.log(video[i]);
-    if (!video[i].paused) {
-      video[i].pause();
-    } else {
-      video[i].play();
-    }
-  });
+function videoClick(id) {
+  var videoChild = id.children[0];
+  var imgChild = id.children[1];
+  console.log(videoChild, imgChild);
+
+  if (!videoChild.paused) {
+    videoChild.pause();
+    imgChild.style.display = "block";
+  } else {
+    videoChild.play();
+    imgChild.style.display = "none";
+  }
 }
